7549a6113d20803555ea4b197c21d40db68d0544
Atharva Baodhankar <baodhankaratharva@gmail.com>
2026-06-21 13:00:24 +0000

    Initial cloud deployment

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const redis = require("redis");
+
+const app = express();
+
+const client = redis.createClient({
+  url: "redis://redis:6379"
+});
+
+client.connect();
+
+app.get("/", async (req, res) => {
+  const visits = await client.incr("visits");
+
+  res.send(`
+    <h1>Atharva's Server</h1>
+    <h2>Visit Count: ${visits}</h2>
+  `);
+});
+
+app.listen(3000, () => {
+  console.log("Server running");
+});
